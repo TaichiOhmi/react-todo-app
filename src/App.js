a14dda06ece866f2e39c23703d18b0cdbca732b8
TaichiOhmi <o6mi5.ta1@gmail.com>
2022-05-15 12:06:21 +0900

    削除やチェック機能を追加

--- src/App.js
+++ src/App.js
@@ -5,7 +5,28 @@ import FilterButton from "./components/FilterButton";
 import Todo from "./components/Todo";
 
 function App(props) {
+
   const [tasks, setTasks] = useState(props.tasks);
+
+  function toggleTaskCompleted(id){
+    const updatedTasks = tasks.map(task => {
+      // if this task has the same ID as the edited task
+      if (id === task.id) {
+        // use objeect spread to make a new object
+        // whose `completed` prop has been inverted
+        return {...task, completed: !task.completed}
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  }
+
+  function deleteTask(id) {
+    // array.filter() は与えられた条件に合致する全要素から新たな配列を生成する。(条件に合わない要素は取り除かれる。)
+    const remainingTasks = tasks.filter(task => id !== task.id);
+    setTasks(remainingTasks);
+  }
+
   const taskList = tasks.map(task => (
     <Todo 
     id={task.id} 
@@ -13,15 +34,21 @@ function App(props) {
     completed={task.completed}
     key={task.id}
     //reactでは一意な key でコンポーネントを管理する。
+    toggleTaskCompleted={toggleTaskCompleted}
+    deleteTask={deleteTask}
     />
   ));
+
   function addTask(name){
     //配列に追加するnewTaskオブジェクトを作成し、新しいタスクの配列を作成してから、タスクデータのStateを更新する。spread構文を使用して既存の配列をコピーし、最後にオブジェクトを追加します。次に、この配列をsetTasks（）に渡して、Stateを更新。 
     const newTask = {id:'todo-' + nanoid(), name:name, completed:false};
     setTasks([...tasks, newTask]);
   }
+
   const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
+
   const headingText = `${taskList.length} ${tasksNoun} remaining`;
+
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -9,17 +9,30 @@ export default function Todo(props){
             また、JSX属性でブール値（trueおよびfalse）を使用するには、それらを中括弧で囲む必要がある。 defaultChecked = "true"と書くと、defaultCheckedの値は "true"の文字列リテラルになってしまう。これではJavaScriptであり、HTMLではなくなってしまう。
             上にあるaria-pressed属性の値は「true」だが、aria-pressedは、真のブール属性ではないから。
             */}
-            <input id={props.id} type="checkbox" defaultChecked={props.completed} />
+            <input 
+                id={props.id} 
+                type="checkbox" 
+                defaultChecked={props.completed} 
+                onChange={() => props.toggleTaskCompleted(props.id)}
+            />
             {/* htmlFor属性はHTMLで使用されるforと同じ役割。JSXの属性としてforを使用することはできないので用いる。 */}
             <label className="todo-label" htmlFor={props.id}>
               {props.name}
             </label>
           </div>
           <div className="btn-group">
-            <button type="button" className="btn">
+            <button 
+                type="button" 
+                className="btn"
+                
+            >
               Edit <span className="visually-hidden">{props.name}</span>
             </button>
-            <button type="button" className="btn btn__danger">
+            <button 
+                type="button" 
+                className="btn btn__danger"
+                onClick={() => props.deleteTask(props.id)}
+            >
               Delete <span className="visually-hidden">{props.name}</span>
             </button>
           </div>
